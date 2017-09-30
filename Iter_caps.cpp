5b1a81054fcf8c9dd516cf11d2ca01bc814c5ce8
Sean <jesky.sean@outlook.com>
2017-09-30 15:13:29 -0600

    Add files via upload

---
+++ Iter_caps.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: sean
+ * Read 'till you don't understand. Then write it 'till you do.
+ * 
+ * Created on September 24, 2017, 7:22 AM
+ */
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
+using std::vector;
+
+int main() {
+    vector<string>ivec = {"Sean Jesky is my name and -\n"
+        "This is written in lowercase"};
+    for (auto it = ivec.begin(); it != ivec.end() && !it -> empty(); it++) {
+        for (auto iter = it -> begin(); iter != it -> end(); iter++) 
+        {
+            *iter = toupper(*iter);
+            cout << *iter;
+        }
+
+    }
+
+    return 0;
+}
+
